efa9516c1455cde7c6db2fe5f8ed8b389baf3077
wangheng <673602165@qq.com>
2019-04-02 16:45:52 +0800

    tensorflow-->layers model

--- trash/tf_model.py
+++ trash/tf_model.py
@@ -6,6 +6,11 @@ def build_model():
         x = tf.placeholder(tf.float32, [None, 200, 200, 3], "x")
         y = tf.placeholder(tf.float32, [None, 4], "y")
 
-    conv1 = tf.layers.conv2d(x, 64, [3, 3], activation=tf.nn.relu, name='conv1')
-    max1 = tf.layers.max_pooling2d(conv1, [2,2], [2,2])
-    conv2 = tf.layers.conv2d(max1)
+    with tf.name_scope("conv1"):
+        conv1 = tf.layers.conv2d(x, 64, [3, 3], activation=tf.nn.relu, name='conv1')
+        max1 = tf.layers.max_pooling2d(conv1, [2,2], [2,2])
+        bn1 = tf.layers.batch_normalization(max1, name='bn1')
+        tf.layers.dropout(bn1, name='droput')
+
+    with tf.name_scope('conv2'):
+        conv2 = tf.layers.conv2d(max1)
